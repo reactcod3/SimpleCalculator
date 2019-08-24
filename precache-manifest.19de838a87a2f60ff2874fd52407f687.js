f74b42b3b1628248f54bac4fa105d9ccb0be4bba
javapyscript <chaitanya.bangera@gmail.com>
2019-08-24 09:38:24 +0530

    Updates

---
+++ precache-manifest.19de838a87a2f60ff2874fd52407f687.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d1826c3577856e1516e1ae2986fc8f8",
+    "url": "/SimpleCalculator/index.html"
+  },
+  {
+    "revision": "4da6ef9f522a988ad718",
+    "url": "/SimpleCalculator/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "26a9c36aa46b534e2c8e",
+    "url": "/SimpleCalculator/static/css/main.9cd1acb3.chunk.css"
+  },
+  {
+    "revision": "4da6ef9f522a988ad718",
+    "url": "/SimpleCalculator/static/js/2.f6b4850d.chunk.js"
+  },
+  {
+    "revision": "26a9c36aa46b534e2c8e",
+    "url": "/SimpleCalculator/static/js/main.ea8d3148.chunk.js"
+  },
+  {
+    "revision": "268356f5a0803fe6bd1a",
+    "url": "/SimpleCalculator/static/js/runtime~main.710fb978.js"
+  }
+]);
